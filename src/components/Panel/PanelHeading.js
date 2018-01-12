6e846839f5b01bd3fbe1afe540856c60894c83f3
Wenbiao Li <lixuyao.tony@gmail.com>
2018-01-12 04:43:11 -0500

    http

--- src/components/Panel/PanelHeading.js
+++ src/components/Panel/PanelHeading.js
@@ -5,7 +5,7 @@ import "./Panel.css";
 export const PanelHeading = props => 
     <div className="panel-heading">
         <center>
-            <img src="https://i67.tinypic.com/2dhz3p4.png" alt="ResCall Logo" />
+            <img src="http://i67.tinypic.com/2dhz3p4.png" alt="ResCall Logo" />
         </center>
     </div>
 
