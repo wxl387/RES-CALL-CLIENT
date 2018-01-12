5243c12a88e43fb742ebfec56e17afb2d91ae98d
Wenbiao Li <lixuyao.tony@gmail.com>
2018-01-12 04:53:43 -0500

    correction

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default {
 
     add: function(userData) {
-        return axios.post("https://res-call-client.herokuapp.com/api/adduser", userData);
+        return axios.post("https://res-call-server.herokuapp.com/api/adduser", userData);
     },
 
     checkpwd: function(userData) {
