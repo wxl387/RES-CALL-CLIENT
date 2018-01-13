3c0c79cafbdffaa2dbfd15b0b7ea700463feba80
Wenbiao Li <lixuyao.tony@gmail.com>
2018-01-12 21:33:52 -0500

    delete home page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
-import MainPage from "./pages/MainPage";
+// import MainPage from "./pages/MainPage";
 
 const App = () =>
   <Router>
     <div>
       <Switch>
-        <Route exact path="/" component={HomePage} />
+        <Route exact path="/" component={LoginPage} />
         <Route exact path="/RegisterPage" component={RegisterPage} />
         <Route exact path="/LoginPage" component={LoginPage} />
         <Route exact path="/MainPage" component={MainPage} />
